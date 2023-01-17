a8dd165c129d4c7ae5baf0ab587445577e8e771f
EveryGameSPlay <repsbro@gmail.com>
2023-01-17 10:33:52 +0300

    Refactored code and updated generation functions

--- ui/mods/map-generator/new_game.js
+++ ui/mods/map-generator/new_game.js
@@ -2,12 +2,20 @@ function getRandomSeed() {
     return Math.floor(65536 * Math.random());
 }
 
-// min and max is inclusive
+// min and max is inclusive (tatarstan)
 function getRandomInt(min, max) {
+
+    if (min === max)
+        return max;
+
     min = Math.ceil(min);
-    max++;
+    max++; // make max inclusive (tatarstan)
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive (stackoverflow)
+}
+
+function getRandomIndex(length) {
+    Math.floor(Math.random() * length);
 }
 
 $(function () {
@@ -74,64 +82,46 @@ $(function () {
     arenaContainer.append(arenaButton2);
     arenaContainer.append(arenaButton3);
 
+    var specificContainer = $('<div></div>')
+    var wadiyaButton = createButton("Wadiya", "generateWadiya")
+    var wateriyaButton = createButton("Wateriya", "generateWateriya")
+    specificContainer.append(wadiyaButton)
+    specificContainer.append(wateriyaButton)
+
 
     addToTable($('<label class="section_title_inner_duel"><b>Duel:</b></label>'))
     addToTable(duelContainer)
     addToTable($('<label class="section_title_inner_arena"><b>FFA:</b></label>'))
     addToTable(arenaContainer)
     addToTable($('<label class="section_title_inner_wadiya"><b>Specific:</b></label>'))
-    addToTable(createButton('Wadiya', 'generateWadiya'))
+    addToTable(specificContainer)
 
     ko.applyBindings(model, duelContainer);
     ko.applyBindings(model, arenaContainer);
+    ko.applyBindings(model, specificContainer);
 })
 
 // TEMPLATES
-model.generateJebus = function () {
+function generateJebus() {
     // var sys = getRandomSystem('Jebus', [380, 500], getRandomBiome(), [30, 30])
 
-    getRandomSystem('Jebus', [380, 500], getRandomBiome(), [30, 30]).then(function(system){
+    getRandomSystem('Jebus', [380, 500], getRandomBiome(), [30, 30]).then(function (system) {
         model.system(system);
         model.updateSystem(model.system());
         model.changeSettings();
         model.requestUpdateCheatConfig();
     });
 }
-generateMacro = function () { 
-    getRandomSystem('Macro', [450, 600], getRandomBiome(), [40, 50]).then(function(system){
+function generateMacro() {
+    getRandomSystem('Macro', [450, 600], getRandomBiome(), [40, 50]).then(function (system) {
         model.system(system);
         model.updateSystem(model.system());
         model.changeSettings();
         model.requestUpdateCheatConfig();
     });
 }
-generateTitans = function () { 
-    getRandomSystem('Titans', [500, 700], getRandomBiome(), [50, 60]).then(function(system){
-        model.system(system);
-        model.updateSystem(model.system());
-        model.changeSettings();
-        model.requestUpdateCheatConfig();
-    });
-}
-
-generateBerserk = function () { 
-    getRandomSystem('Berserk', [380, 500], getRandomBiome(), [35, 40]).then(function(system){
-        model.system(system);
-        model.updateSystem(model.system());
-        model.changeSettings();
-        model.requestUpdateCheatConfig();
-    });
-}
-generateArena = function () {
-    getRandomSystem('Arena', [450, 650], getRandomBiome(), [40, 50]).then(function(system){
-        model.system(system);
-        model.updateSystem(model.system());
-        model.changeSettings();
-        model.requestUpdateCheatConfig();
-    });
- }
-generateBattlefield = function () { 
-    getRandomSystem('Battlefield', [600, 1300], getRandomBiome(), [50, 60]).then(function(system){
+function generateTitans() {
+    getRandomSystem('Titans', [500, 700], getRandomBiome(), [50, 60]).then(function (system) {
         model.system(system);
         model.updateSystem(model.system());
         model.changeSettings();
@@ -139,42 +129,124 @@ generateBattlefield = function () {
     });
 }
 
-generateWadiya = function () { 
-    getRandomSystem('Wadiya', [600, 800], "moon", [80, 100]).then(function(system){
+function generateBerserk() {
+    getRandomSystem('Berserk', [380, 500], getRandomBiome(), [35, 40]).then(function (system) {
         model.system(system);
         model.updateSystem(model.system());
         model.changeSettings();
         model.requestUpdateCheatConfig();
     });
+}
+function generateArena() {
+    getRandomSystem('Arena', [450, 650], getRandomBiome(), [40, 50]).then(function (system) {
+        model.system(system);
+        model.updateSystem(model.system());
+        model.changeSettings();
+        model.requestUpdateCheatConfig();
+    });
+}
+function generateBattlefield() {
+    getRandomSystem('Battlefield', [600, 1300], getRandomBiome(), [50, 60]).then(function (system) {
+        model.system(system);
+        model.updateSystem(model.system());
+        model.changeSettings();
+        model.requestUpdateCheatConfig();
+    });
+}
+
+function generateWadiya() {
+    getRandomSystem('Wadiya', [600, 800], moonBiome, [80, 100],
+        createGenerationOptions(undefined, 0, undefined)).then(function (system) {
+            model.system(system);
+            model.updateSystem(model.system());
+            model.changeSettings();
+            model.requestUpdateCheatConfig();
+        });
+}
+function generateWateriya() {
+    getRandomSystem('Wateriya', [600, 800], getRandomBiome([earthBiome, tropicalBiome]), [80, 100],
+        createGenerationOptions(undefined, 70, undefined)).then(function (system) {
+            model.system(system);
+            model.updateSystem(model.system());
+            model.changeSettings();
+            model.requestUpdateCheatConfig();
+        });
 }
 // TEMPLATES END
 
-getRandomBiome = function () {
-    const biomes = ["earth", "desert", "lava", "tropical", "moon", "metal"];
+// FUNCTIONS
+const earthBiome = "earth"
+const desertBiome = "desert"
+const lavaBiome = "lava"
+const tropicalBiome = "tropical"
+const moonBiome = "moon"
+const metalBiome = "metal"
 
-    const random = Math.floor(Math.random() * biomes.length);
-    console.log(random, biomes[random]);
-    return biomes[random]
+function getAllPossibleBiomes() {
+    const defaultBiomes = [earthBiome, desertBiome, lavaBiome, tropicalBiome, moonBiome, metalBiome]
+    return defaultBiomes
 }
 
-getRandomSystem = function (planetTitle, planetRadiusRange, biomeName, metalFactorRange) {
-    console.log(biomeName +' passed to the generation')
+function getRandomBiome(biomes = undefined) {
+    if (biomes === undefined)
+        biomes = getAllPossibleBiomes();
+
+    const random = getRandomIndex(biomes.length);
+    console.log(random, biomes[random]);
+    return biomes[random];
+}
+
+function getOneOfWaterGenerationOption() {
+    const types = ["random", "nowater", "muchwater"]
+    const type = types[getRandomIndex(types.length)]
+
+    switch (type) {
+        case "random": return getRandomInt(0, 50); break;
+        case "nowater": return 0; break;
+        case "muchwater": return getRandomInt(50, 70); break;
+    }
+    return 0
+}
+
+// Use undefined to get random value
+function createGenerationOptions(temperature, waterDepth, height) {
+    if (temperature === undefined)
+        temperature = getRandomInt(0, 100)
+    if (waterDepth === undefined)
+        waterDepth = getOneOfWaterGenerationOption();
+    if (height === undefined)
+        height = getRandomInt(0, 60);
+    return { temperature: temperature, waterDepth: waterDepth, height: height }
+}
+
+getRandomSystem = function (planetTitle, planetRadiusRange, biomeName, metalFactorRange,
+    generationOptions = undefined) {
+
+    if (generationOptions === undefined)
+        generationOptions = createGenerationOptions(undefined, undefined, undefined)
+
+    console.log(biomeName + ' passed to the generation')
+
+    // Gameplay
     var seed = getRandomSeed();
     var radius = getRandomInt(planetRadiusRange[0], planetRadiusRange[1]);
     var metalFactor = getRandomInt(metalFactorRange[0], metalFactorRange[1]);
 
-    var temperature = getRandomInt(0, 100);
-    var waterDepth = getRandomInt(0, 60);
-    var height = getRandomInt(0, 60);
+    // Planet surface
+    const temperature = generationOptions.temperature;
+    const waterDepth = generationOptions.waterDepth;
+    const height = generationOptions.height;
 
+    // Orbit parameter, does nothing but need to initialize
     var mass = 10000;
 
+    // Players
     var slots = model.slots() || 2;
 
-    var metalClustersRandom = Math.floor(slots / 10 * 50);
+    var metalSlotsMultiplier = Math.floor(slots / 10 * 50);
 
-    var metalDensity = metalFactor;
-    var metalClusters = 50 + metalClustersRandom;
+    var metalDensity = metalFactor + metalSlotsMultiplier;
+    var metalClusters = 50 + metalSlotsMultiplier;
 
     var planet = {
         mass: mass,
@@ -207,7 +279,7 @@ getRandomSystem = function (planetTitle, planetRadiusRange, biomeName, metalFact
     }
 
     var rSystem = {
-        name: planetTitle + ' ' + metalClusters + ' ' + metalDensity,
+        name: planetTitle + ' C' + metalClusters + ' D' + metalDensity,
         isRandomlyGenerated: true,
         players: [slots, slots]
     };
